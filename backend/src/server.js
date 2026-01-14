a0cd1860919cbb59e00a1d6485d975492ff8ed4a
0912-Avisek <abhikrq@gmail.com>
2026-01-15 02:41:48 +0530

    db connection done

---
+++ backend/src/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import authRouter from './routes/auth.route.js';
+import { connectDB }from './lib/db.js';
+
+
+const app = express();
+
+dotenv.config();
+
+app.use(express.json());
+
+app.use("/auth",authRouter);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+    connectDB();
+});
